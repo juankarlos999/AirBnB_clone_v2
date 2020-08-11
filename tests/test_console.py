b492f9fc2ad0b2cd1fbfee2bf8a0bf955e30872b
juankarlos999 <juan_karlos_999@hotmail.com>
2020-08-11 16:20:25 -0500

    test console

---
+++ tests/test_console.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Test for console"""
+import unittest
+import pep8
+import tests
+from console import HBNBCommand
+
+class TestConsole(unittest.TestCase):
+    """this will test the console"""
+
+    @classmethod
+    def setUpClass(cls):
+        """setup test"""
+        cls.consol = HBNBCommand()
+
+    def test_pep8_conformance(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['console.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+if __name__ == "__main__":
+    unittest.main()
