1fbfd05c505e02de776f92ac2e3dee5d58d561eb
juankarlos999 <juan_karlos_999@hotmail.com>
2020-08-11 16:29:17 -0500

    test console

--- tests/test_console.py
+++ tests/test_console.py
@@ -17,8 +17,7 @@ class TestConsole(unittest.TestCase):
         """Test that we conform to PEP8."""
         pep8style = pep8.StyleGuide(quiet=True)
         result = pep8style.check_files(['console.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
+        self.assertEqual(result.total_errors, 0, 'fix Pep8'
 
 if __name__ == "__main__":
     unittest.main()
