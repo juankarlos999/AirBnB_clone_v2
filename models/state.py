a58c30f48c4031c4457aa436b9ba5372d0b2aaaf
juankarlos999 <juan_karlos_999@hotmail.com>
2020-08-12 01:29:59 -0500

    pep8

--- models/state.py
+++ models/state.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
-from sqlalchemy import (Column, ForeignKey, String)
+
+from models.base_model import BaseModel, Base
+from sqlalchemy import (Column, ForeignKey, String, Integer)
 from sqlalchemy.orm import relationship
 from models.city import City
 import os
