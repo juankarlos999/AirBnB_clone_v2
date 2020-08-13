ee0c86e6574e6ab03ef4acdfe32d312f0a002cd2
andres reyes <51345583+andresvanegas19@users.noreply.github.com>
2020-08-13 16:44:38 -0500

    add a parentesis

--- models/state.py
+++ models/state.py
@@ -17,7 +17,7 @@ class State(BaseModel, Base):
 
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete"
+    cities = relationship("City", backref="state", cascade="all, delete")
 
 
     if getenv("HBNB_TYPE_STORAGE") != "db":
