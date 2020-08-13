8c7e882a96b4dff73ada6e211106dc7939c154f3
juankarlos999 <juan_karlos_999@hotmail.com>
2020-08-13 17:28:11 -0500

    [ADD]: new attribute place_amenities

--- models/amenity.py
+++ models/amenity.py
@@ -3,11 +3,11 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, MetaData
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship, backref
-
+from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
     """The amenaty of the place"""
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-
+    place_amenities = relationship("Place", secondary=place_amenity)
