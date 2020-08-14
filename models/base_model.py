2556ac525ac9314e0975912c1bff47279deffc14
juankarlos999 <juan_karlos_999@hotmail.com>
2020-08-13 22:46:51 -0500

    models.storage.delete(self)

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,7 @@
 import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import (Column, DateTime, Stringi, MetaData)
+from sqlalchemy import (Column, DateTime, String, MetaData)
 import models
 
 
