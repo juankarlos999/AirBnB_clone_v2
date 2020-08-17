66f33c9ab84308e80b6794d529cf83000874a9a6
andres reyes <andressantiagore@gmail.com>
2020-08-17 17:27:51 -0500

    make executable the file and upload

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -11,17 +11,14 @@ content="<html>
 
 location="        location \/hbnb_static/ { alias /data/web_static/current/; }"
 
-if ! which nginx >/dev/null 2>&1; then
-  sudo apt-get -y update
-  sudo apt-get -y upgrade
-  sudo apt-get -y install nginx
-fi
+sudo apt-get -y update
+sudo apt-get -y install nginx
 
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
 echo "$content" >/data/web_static/releases/test/index.html
 ln -sfn /data/web_static/current /data/web_static/releases/test/
-chown -hR ubuntu:ubuntu /data/
+chown -R ubuntu:ubuntu /data
 
 sudo sed -i "55 i\ $location" /etc/nginx/sites-available/default
 # restart nginx
