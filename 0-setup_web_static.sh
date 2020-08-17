00d383250e93f34e88b8c3162d88d0caa063ee59
andres reyes <andressantiagore@gmail.com>
2020-08-17 18:35:49 -0500

    improve the scropt

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,16 +6,20 @@ content="<html> <head> </head> <body> Holberton School </body> </html>"
 location=" location \/hbnb_static/ { alias /data/web_static/current/; }"
 
 if ! which nginx > /dev/null 2>&1; then
-    apt update && sudo apt-get install -y nginx
+  apt update && apt-get install -y nginx
 fi
 
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
 echo "$content" > /data/web_static/releases/test/index.html
 mkdir -p /data/web_static/current
-ln -sfn /data/web_static/current /data/web_static/releases/test/
-chown -R ubuntu:ubuntu /data
+if [ -d "/data/web_static/current" ]; then
+  rm -r /data/web_static/current
+fi
+ln -sfn /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
 
-sed -i "55 i\ $location" /etc/nginx/sites-available/default
+sed -i "40i\ $location" /etc/nginx/sites-available/default
 # restart nginx
 service nginx restart > /dev/null 2>&1
+
