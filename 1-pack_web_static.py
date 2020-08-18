f3055aea26873489ae8d03e9276f4c41eb5ed5c5
andres reyes <andressantiagore@gmail.com>
2020-08-17 20:03:48 -0500

    upload script to automate the uploading files

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,4 +1,27 @@
 #!/usr/bin/python3
 """ Automate the service on the server """
+from datetime import datetime
+from fabric.api import (local, hide)
 
 
+def do_pack():
+    """ generates a .tgz archive from the contents
+    of the web_static folder of your AirBnB Clone
+    repo, using the function do_pack """
+    # file is the name of the file it will compress
+    files = datetime.now().strftime("versions/web_static_%Y%m%d%H%M%S.tgz")
+    with hide('output', 'running'):
+        local('mkdir versions')
+
+    print('Packing web_static to {} web_static'.format(files))
+
+    # with hide('stdout', 'running'):
+    local('tar -cvzf {} web_static'.format(files))
+
+    with hide('output', 'running'):
+        size = local("wc -c {}".format(files), capture=True)
+        size = size.stdout.split()[0]
+
+    print("web_static packed: {} -> {}Bytes".format(files, size))
+
+    return files
