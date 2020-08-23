93f41a21777dd9241cf2dd5b6b6f2ca81cdcf37f
juankarlos999 <juan_karlos_999@hotmail.com>
2020-08-22 21:27:30 -0500

    script that sets up your web servers for the deployment

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,25 +1,17 @@
 #!/usr/bin/env bash
 # sets up your web servers for the deployment of web_static
 
-content="<html> <head> </head> <body> Holberton School </body> </html>"
-
-location=" location \/hbnb_static/ { alias /data/web_static/current/; }"
-
-if ! which nginx > /dev/null 2>&1; then
+if ! which nginex;
+then
   apt update && apt-get install -y nginx
 fi
 
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
-echo "$content" > /data/web_static/releases/test/index.html
-mkdir -p /data/web_static/current
-if [ -d "/data/web_static/current" ]; then
-  rm -r /data/web_static/current
-fi
-ln -sfn /data/web_static/releases/test/ /data/web_static/current
+rm -rf /data/web_static/current
+echo "test Nginx configuration" > /data/web_static/releases/test/index.html
+ln -s /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-
+location="\\n\tlocation \/hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
 sed -i "40i\ $location" /etc/nginx/sites-available/default
-# restart nginx
-service nginx restart > /dev/null 2>&1
-
+sudo /etc/init.d/nginx start
