31e68d17f637c9c29662f0fa6facc7dc10dbe1f9
juankarlos999 <juan_karlos_999@hotmail.com>
2020-08-23 10:36:24 -0500

    Fabric script that generates a .tgz archive

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,35 +1,25 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """ Automate the service on the server """
 from datetime import datetime
-from fabric.api import (local, hide, settings)
+from fabric.api import *
 
 
 def do_pack():
-    """ generates a .tgz archive from the contents
+    """
+    generates a .tgz archive from the contents
     of the web_static folder of your AirBnB Clone
-    repo, using the function do_pack """
-    # file is the name of the file it will compress
-    with settings(warn_only=True):
-        files = datetime.now().strftime("versions/web_static_%Y%m%d%H%M%S.tgz")
-        with hide('output', 'running'):
-            command = local('mkdir versions')
-
-        if command.return_code != 0:
-            return None
-
-        print('Packing web_static to {} web_static'.format(files))
-
-        # with hide('stdout', 'running'):
-        command = local('tar -cvzf {} web_static'.format(files))
-
-        if command.return_code != 0:
-            return None
-
-        with hide('output', 'running'):
-            size = local("wc -c {}".format(files), capture=True)
-            if size.return_code != 0:
-                return None
-            size = size.stdout.split()[0]
-
-        print("web_static packed: {} -> {}Bytes".format(files, size))
-        return files
+    repo, using the function do_pack
+    """
+    file_name = datetime.now().strftime("versions/web_static_%Y%m%d%H%M%S.tgz")
+    print("Packing web_static to {}".format(file_name))
+    try:
+        with hide('output', 'running', 'warnings'):
+            local('mkdir -p versions')
+        local("tar -czvf {} web_static".format(file_name))
+        with hide('output', 'running', 'warnings'):
+            size_bytes = local("wc -c {}".format(file_name), capture=True)
+        bytes_ = size_bytes.split()
+        print("web_static packed: {} -> {}Bytes".format(file_name, bytes_[0]))
+        return file_name
+    except:
+        return None
