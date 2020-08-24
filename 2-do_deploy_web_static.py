38e62989fbc181cf8763688e6b424efca6aa1f39
juankarlos999 <juan_karlos_999@hotmail.com>
2020-08-24 17:27:49 -0500

    Fabric script that distributes an archive to a web servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -36,16 +36,20 @@ def do_deploy(archive_path):
         return False
     file_tgz = archive_path.split('/')
     file_name = file_tgz[1].split('.')
-    put(archive_path, '/tmp/{}'.format(file_tgz[1]))
-    run('mkdir -p /data/web_static/releases/{}/'.format(file_name[0]))
-    run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'
-        .format(file_tgz[1], file_name[0]))
-    run('rm /tmp/{}'.format(file_tgz[1]))
-    run('mv /data/web_static/releases/{}/web_static/*\
+    try:
+        put(archive_path, '/tmp/{}'.format(file_tgz[1]))
+        run('mkdir -p /data/web_static/releases/{}/'.format(file_name[0]))
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'
+            .format(file_tgz[1], file_name[0]))
+        run('rm /tmp/{}'.format(file_tgz[1]))
+        run('mv /data/web_static/releases/{}/web_static/*\
         /data/web_static/releases/{}/'.format(file_name[0], file_name[0]))
-    run('rm -rf /data/web_static/releases/{}/web_static'.format(file_name[0]))
-    run('rm -rf /data/web_static/current')
-    run('ln -s /data/web_static/releases/{}/ \
-    /data/web_static/current'.format(file_name[0]))
-    print('New version deployed!')
-    return True
+        run('rm -rf /data/web_static/releases/{}/web_static'
+            .format(file_name[0]))
+        run('rm -rf /data/web_static/current')
+        run('ln -s /data/web_static/releases/{}/ \
+        /data/web_static/current'.format(file_name[0]))
+        print('New version deployed!')
+        return True
+    except:
+        return False
