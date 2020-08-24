256c4006add4434dd8ee4d2193146dcf85fabd94
juankarlos999 <juan_karlos_999@hotmail.com>
2020-08-24 17:42:39 -0500

    Fabric script that distributes an archive to a web servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -32,24 +32,22 @@ def do_deploy(archive_path):
     Fabric script that distributes an archive to a web servers
     """
     if not os.path.exists(archive_path):
-        print("not exist PATH")
         return False
+
     file_tgz = archive_path.split('/')
     file_name = file_tgz[1].split('.')
-    try:
-        put(archive_path, '/tmp/{}'.format(file_tgz[1]))
-        run('mkdir -p /data/web_static/releases/{}/'.format(file_name[0]))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'
-            .format(file_tgz[1], file_name[0]))
-        run('rm /tmp/{}'.format(file_tgz[1]))
-        run('mv /data/web_static/releases/{}/web_static/*\
-        /data/web_static/releases/{}/'.format(file_name[0], file_name[0]))
-        run('rm -rf /data/web_static/releases/{}/web_static'
-            .format(file_name[0]))
-        run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{}/ \
+
+    put(archive_path, '/tmp/{}'.format(file_tgz[1]))
+    run('mkdir -p /data/web_static/releases/{}/'.format(file_name[0]))
+    run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'
+        .format(file_tgz[1], file_name[0]))
+    run('rm /tmp/{}'.format(file_tgz[1]))
+    run('mv /data/web_static/releases/{}/web_static/*\
+    /data/web_static/releases/{}/'.format(file_name[0], file_name[0]))
+    run('rm -rf /data/web_static/releases/{}/web_static'
+        .format(file_name[0]))
+    run('rm -rf /data/web_static/current')
+    run('ln -s /data/web_static/releases/{}/ \
         /data/web_static/current'.format(file_name[0]))
-        print('New version deployed!')
-        return True
-    except:
-        return False
+    print('New version deployed!')
+    return True
